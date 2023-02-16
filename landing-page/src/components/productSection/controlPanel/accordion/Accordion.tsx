90616f53ebc5fd80eb234c2c0ddc172125620811
Martyna Obrebska <martyna.obrebska94@gmail.com>
2023-02-16 22:47:55 +0100

    Add accordion animation functionality.

--- landing-page/src/components/productSection/controlPanel/accordion/Accordion.tsx
+++ landing-page/src/components/productSection/controlPanel/accordion/Accordion.tsx
@@ -1,39 +1,46 @@
 import { useState } from "react";
+import AccordionTab from "./accordionTab/AccordionTab";
 
 function Accordion() {
   const [activeLabel, setActiveLabel] = useState<number | null>(null);
-  const labels = [
-    "Sizes",
-    "Colors",
-    "Storage features",
-    "Materials",
-    "Delivery & Assembly",
-  ];
-  const texts = [
-    "Security: The ability for the website to protect against hacking and other security threats, ensuring that user data. Security: The ability for the website to protect against hacking and other security threats, ensuring that user data.",
-    "Security: The ability for the website to protect against hacking and other security threats, ensuring that user data. Security: The ability for the website to protect against hacking and other security threats, ensuring that user data.",
-    "Security: The ability for the website to protect against hacking and other security threats, ensuring that user data. Security: The ability for the website to protect against hacking and other security threats, ensuring that user data.",
-    "Security: The ability for the website to protect against hacking and other security threats, ensuring that user data. Security: The ability for the website to protect against hacking and other security threats, ensuring that user data.",
-    "Security: The ability for the website to protect against hacking and other security threats, ensuring that user data. Security: The ability for the website to protect against hacking and other security threats, ensuring that user data.",
+  const tabs = [
+    {
+      label: "Sizes",
+      content:
+        "Security: The ability for the website to protect against hacking and other security threats, ensuring that user data. Security: The ability for the website to protect against hacking and other security threats, ensuring that user data.",
+    },
+    {
+      label: "Colors",
+      content:
+        "Security: The ability for the website to protect against hacking and other security threats, ensuring that user data. Security: The ability for the website to protect against hacking and other security threats, ensuring that user data.",
+    },
+    {
+      label: "Storage features",
+      content:
+        "Security: The ability for the website to protect against hacking and other security threats, ensuring that user data. Security: The ability for the website to protect against hacking and other security threats, ensuring that user data.",
+    },
+    {
+      label: "Materials",
+      content:
+        "Security: The ability for the website to protect against hacking and other security threats, ensuring that user data. Security: The ability for the website to protect against hacking and other security threats, ensuring that user data.",
+    },
+    {
+      label: "Delivery & Assembly",
+      content:
+        "Security: The ability for the website to protect against hacking and other security threats, ensuring that user data. Security: The ability for the website to protect against hacking and other security threats, ensuring that user data.",
+    },
   ];
 
   return (
     <div className="accordion">
-      {labels.map((label, index) => {
-        const isActive = activeLabel === index;
-        const activeToggle = isActive ? " active" : "";
-        const handleClickLabel = () => setActiveLabel(isActive ? null : index);
-
-        return (
-          <div key={index} className="info">
-            <div className="label" onClick={handleClickLabel}>
-              <div className={`plus${activeToggle}`} />
-              <p className={`labelName${activeToggle}`}>{label}</p>
-            </div>
-            <div className={`text${activeToggle}`}>{texts[index]}</div>
-          </div>
-        );
-      })}
+      {tabs.map((tab, index) => (
+        <AccordionTab
+          index={index}
+          tab={tab}
+          activeLabel={activeLabel}
+          setActiveLabel={setActiveLabel}
+        />
+      ))}
     </div>
   );
 }
